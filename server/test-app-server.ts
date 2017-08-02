ee2c117da1c0a3f6d8d89356cb10832c29af9c93
Paul Wooten <pawooten@gmail.com>
2017-08-02 08:27:32 -0400

    Added test-app-server.ts, updated npm start script to start ng serve and node test-app-server.js concurrently

---
+++ server/test-app-server.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import * as path from 'path';
+import {Server} from 'ws';
+
+const app = express();
+
+// Serve http content
+// extra comment
+app.get('/', (request, response) => {
+  console.log('get / ');
+    response.sendFile(path.join(__dirname, '../client/client.html'));
+});
+
+app.use('/', express.static(path.join(__dirname, '..', 'client')));
+app.use('/node_modules', express.static(path.join(__dirname, '..', 'node_modules')));
+
+const httpServer = app.listen(8000, 'localhost', () => {
+    const {address, port} = httpServer.address();
+    console.log('HTTP Server is listening on %s %s', address, port);
+});
+
+// WebSocket Server
+const wsServer: Server = new Server({port: 8085});
+console.log('WebSocket server is listening on port 8085');
+wsServer.on('connection', webSocket => {
+  webSocket.send('This message was pushed by the WebSocket server');
+});
